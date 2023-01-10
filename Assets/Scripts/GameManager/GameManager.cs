3ab4092cbeb742aae5d8fb5c7bbdc213935d9a7d
JonatasWillian <91629214+JonatasWillian@users.noreply.github.com>
2023-01-09 21:45:40 -0300

    gamemanager

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("List Players")]
+    public Player[] listPlayers;
+
     [Header("Ball Base")]
     public BallBase ballBase;
     public float timeSetBallFree = 1f;
 
+    [Header("Menus")]
+    public GameObject uiMainMenu;
+
+    [Header("StateMachine")]
     public StateMachine stateMachine;
 
     public static GameManager Instance;
 
+    private bool _blocked;
+
     private void Awake()
     {
         Instance = this;
+
+        listPlayers = FindObjectsOfType<Player>();
     }
 
     public void ResetBall()
@@ -24,13 +35,35 @@ public class GameManager : MonoBehaviour
         Invoke(nameof(SetBallFree), timeSetBallFree);
     }
 
+    public void ResetPlayers()
+    {
+        foreach (var player in listPlayers)
+        {
+            player.ResetPlayer();
+        }
+    }
+
     private void SetBallFree()
     {
+        if (_blocked) return;
         ballBase.CanMove(true);
     }
 
     public void StartGame()
     {
         ballBase.CanMove(true);
+        _blocked = false;
+    }
+
+    public void EndGame()
+    {
+        stateMachine.SwitchState(StateMachine.States.END_GAME);
+        _blocked = true;
+    }
+
+    public void ShowMainMenu()
+    {
+        uiMainMenu.SetActive(true);
+        ballBase.CanMove(false);
     }
 }
