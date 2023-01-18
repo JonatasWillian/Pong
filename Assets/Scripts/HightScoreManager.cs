3bf53e07ea5703c1e54cb7b6d3858e08a344f684
JonatasWillian <91629214+JonatasWillian@users.noreply.github.com>
2023-01-18 17:23:20 -0300

    script HighScore

---
+++ Assets/Scripts/HightScoreManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HightScoreManager : MonoBehaviour
+{
+    [Header("Text")]
+    public TextMeshProUGUI uiTextName;
+
+    public static HightScoreManager Instance;
+
+    private string keyToSave = "KeyToSave";
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void OnEnable()
+    {
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        uiTextName.text = PlayerPrefs.GetString(keyToSave, "Nothing");
+    }
+}
